40fbcb2bd689e5b0a877c0df294e211bd2e4ce99
Evgeniy Kulikov <im@kulikov.im>
2016-12-10 01:50:06 +0300

    fixes

--- renew_cert.sh
+++ renew_cert.sh
@@ -1,5 +1,8 @@
-cd /home/welefen/www/www.thinkjs.org/ssl
-python acme_tiny.py --account-key account.key --csr domain.csr --acme-dir /home/welefen/www/www.thinkjs.org/www/challenges/ > signed.crt || exit
+cd ./ssl
+mkdir -p ../www/challenges/
+openssl genrsa 4096 > account.key
+openssl req -new -newkey rsa:4096 -nodes -keyout ./private.key -out ./domain.csr -config ./openssl.cnf
+python acme_tiny.py --account-key account.key --csr domain.csr --acme-dir ../www/challenges/ > signed.crt || exit
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
 cat signed.crt intermediate.pem > chained.pem
 service nginx reload
